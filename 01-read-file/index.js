daa48dec32ff13ab3572465f1ac211bf6cee9039
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-02 16:40:46 +0300

    feat: implement 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let fileStream = fs.createReadStream('01-read-file/text.txt', 'utf8');
+
+fileStream.on("data", function(chunk){ 
+  console.log(chunk);
+});
+
+// const Emitter = require('events');
+// let emitter = new Emitter();
+
+// let eventName = 'greet';
+
+// emitter.on(eventName, function (data) {
+//   console.log(data);
+// });
+
+// emitter.on(eventName, function () {
+//   console.log('Привет!');
+// });
+
+// emitter.emit(eventName, "HELLO CRUEL WORLD!");
