297874c7aefa5f4ddd167d0f43cf9067c50c2047
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-04 14:10:19 +0300

    feat: implement 02-write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let fileStream = fs.createReadStream('01-read-file/text.txt', 'utf8');
 
-fileStream.on("data", function(chunk){ 
+fileStream.on("data", function(chunk) {
   console.log(chunk);
 });
 
