1f83e6fb84557785471391dd71d064f146113e5a
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-04 15:23:48 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+
+const fs = require('fs');
+
+const path = __dirname + '\\secret-folder\\';
+
+fs.readdir(path, { withFileTypes: true }, (err, files) => {
+  console.log("\nТекущие файлы:");
+  
+  files.forEach(file => {
+    if (file.isFile()) {
+      
+      fs.stat(path + `\\${file.name}`, (err, fileInfo) => {
+        let fileName = file.name.match(/^\w+./i)[0].slice(0, -1);
+        let fileExt = file.name.match(/.\w+$/i)[0].slice(1);
+        let fileSize = parseFloat(fileInfo.size / 1024).toFixed(3) + " KB";
+
+        console.log(`${fileName} - ${fileExt} - ${fileSize}`);
+      });
+    }
+  });
+});
