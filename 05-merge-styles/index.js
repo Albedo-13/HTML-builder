580a8286a75ad72b483ce59043bf71da1a32b4d9
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-06 16:20:58 +0300

    feat: implement 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,2 +1,19 @@
 
 // https://github.com/EvgeniiMal/HTML-builder/tree/main/05-merge-styles
+
+const fs = require('fs');
+
+const pathCopyFrom = __dirname + '\\styles\\';
+const pathCopyTo = __dirname + '\\project-dist\\';
+let content = "";
+
+fs.readdir(pathCopyFrom, { withFileTypes: true }, (err, files) => {
+  files.forEach(file => {
+    if (file.isFile() && file.name.match(/.\w+$/i)[0].slice(1) === "css") {
+      fs.readFile(`${pathCopyFrom}\\${file.name}`, 'utf8', (err, data) => {
+        content += data;
+        fs.writeFile(`${pathCopyTo}\\bundle.css`, content, () => { });
+      });
+    }
+  });
+});
