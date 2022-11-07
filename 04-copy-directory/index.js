f1c76f336fc875db8ab4a56497accfc1ab94638a
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-07 16:57:44 +0300

    feat: recreating copy directory to remove deleted files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,14 +3,17 @@ const fs = require('fs');
 const pathCopyFrom = __dirname + '\\files\\';
 const pathCopyTo = __dirname + '\\files-copy\\';
 
-fs.mkdir(pathCopyTo, { recursive: true }, () => { });
+fs.rm(pathCopyTo, { recursive: true, force: true }, () => {
+  fs.mkdir(pathCopyTo, { recursive: true }, () => { });
 
-fs.readdir(pathCopyFrom, { withFileTypes: false }, (err, files) => {
-
-  console.log("\nТекущие файлы:");
-  files.forEach(file => {
-    fs.copyFile(pathCopyFrom + `${file}`, pathCopyTo + `${file}`, () => {
-      console.log(`Файл ${file} скопирован`);
+  fs.readdir(pathCopyFrom, { withFileTypes: false }, (err, files) => {
+  
+    console.log("\nТекущие файлы:");
+    files.forEach(file => {
+      fs.copyFile(pathCopyFrom + `${file}`, pathCopyTo + `${file}`, () => {
+        console.log(`Файл ${file} скопирован`);
+      });
     });
   });
 });
+
