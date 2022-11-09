d999d8e72695501ed3d0e1181c022f3aa1f55de7
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-09 21:00:14 +0300

    fix: change file path to work corrent on different OS

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,7 +10,7 @@ fs.readdir(pathToFile, { withFileTypes: true }, (err, files) => {
   files.forEach(file => {
     if (file.isFile()) {
       
-      fs.stat(pathToFile + `\\${file.name}`, (err, fileInfo) => {
+      fs.stat(path.join(pathToFile, file.name), (err, fileInfo) => {
         let fileName = file.name.match(/^\w+./i)[0].slice(0, -1);
         let fileExt = file.name.match(/.\w+$/i)[0].slice(1);
         let fileSize = parseFloat(fileInfo.size / 1024).toFixed(3) + " KB";
