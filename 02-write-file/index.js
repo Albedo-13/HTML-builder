53ee607bbe79f4fc847e41cd1fbad8330345e0ff
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-11-09 21:19:33 +0300

    fix: goodbye message now shown if close script using ctrl+C

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,9 +3,15 @@ const path = require('path');
 
 const { stdin, stdout } = process;
 
+fs.open('02-write-file/text.txt', 'w', () => { });
 stdout.write('Файл открыт для записи\n');
+
 stdin.on('data', data => writeTo(data));
 process.on('exit', () => stdout.write('Запись в файл завершена.'));
+
+process.on('SIGINT', () => {
+  process.exit();
+});
 
 function writeTo (data) {
     const buffer = Buffer.from(data, 'utf8');
